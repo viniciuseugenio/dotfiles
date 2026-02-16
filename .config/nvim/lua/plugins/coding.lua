d37fecbd91640957bbab52ad79d8d2b702f4138e
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-02-15 22:54:35 -0300

    build(nvim): add nvim-java, nvim-spectre and lsp-file-operations

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -119,4 +119,22 @@ return {
       }
     end,
   },
+  { "nvim-java/nvim-java" },
+  {
+    "nvim-pack/nvim-spectre",
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- Uncomment whichever supported plugin(s) you use
+      -- "nvim-tree/nvim-tree.lua",
+      -- "nvim-neo-tree/neo-tree.nvim",
+      -- "simonmclean/triptych.nvim"
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
