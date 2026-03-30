397099e7e28b9b6a9d73b724f9e11c8e466a2fba
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-29 23:37:53 -0300

    style(nvim): change cursor style

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,7 +6,11 @@ local o = vim.o
 -- o.cursorlineopt = "both" -- to enable cursorline!
 o.relativenumber = true
 o.clipboard = ""
-o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait300-blinkon250-blinkoff150"
+
+-- Make cursor full-block style, but change the colors when is on normal or insert mode
+vim.cmd.highlight "iCursor gui=NONE guifg=NONE guibg=#B59E7D"
+vim.cmd.highlight "nCursor gui=NONE guifg=NONE guibg=#922b07"
+o.guicursor = "n-v-c:block-nCursor,i:block-iCursor,r-cr:hor20,o:hor50,a:blinkwait300-blinkon250-blinkoff150"
 
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 --   pattern = "*",
