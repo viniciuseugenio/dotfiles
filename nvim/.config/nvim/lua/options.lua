2bfd0387dfcbea25c8d6e1ac3fed355e52fc73ee
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-05-28 14:10:39 -0300

    style(nvim): make cursor block at every mode

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -10,7 +10,7 @@ o.clipboard = ""
 -- Make cursor full-block style, but change the colors when is on normal or insert mode
 vim.cmd.highlight "iCursor gui=NONE guifg=NONE guibg=#B59E7D"
 vim.cmd.highlight "nCursor gui=NONE guifg=NONE guibg=#922b07"
-o.guicursor = "n-v-c:block-nCursor,i:block-iCursor,r-cr:hor20,o:hor50,a:blinkwait300-blinkon250-blinkoff150"
+o.guicursor = "n-v-c:block-nCursor,i:block-iCursor,r-cr:hor20,o:hor50"
 
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 --   pattern = "*",
@@ -24,6 +24,8 @@ o.guicursor = "n-v-c:block-nCursor,i:block-iCursor,r-cr:hor20,o:hor50,a:blinkwai
 --   vim.b.copilot_suggestion_hidden = true
 -- end)
 --
+
+o.termguicolors = true
 
 vim.api.nvim_set_hl(
   0,
